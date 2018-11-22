c01e5fa99fbcd0dfe71d197168329a2561bef0b9
901786 Cascino Romain <romain.cascino@gmail.com>
2018-11-22 17:08:22 +0700

    Add header for about and resources page, footer rework

--- footer.js
+++ footer.js
@@ -1,14 +1,5 @@
 const footerContent = `
-    <div class="footer-item">
-      <a href="index.html">Home</a>
-    </div>  
-    <div class="footer-item">
-      <a href="about.html">About</a>
-    </div>
-    <div class="footer-item">
-      <a href="resources.html">Resources</a>
-    </div>
-    <div class="footer-item">
-      &#8211; <em>Romain Cascino</em> &#8211; 2018
+    <div class="footer-row">
+      © Romain Cascino &#8211; 2018
     </div>
 `;
---
+++ header.js
@@ -0,0 +1,13 @@
+const headerContent = `
+    <div class="header-item">
+      <a href="index.html">Home</a>
+    </div>
+    <div class="header-item">|</div>
+    <div class="header-item">
+      <a href="about.html">About</a>
+    </div>
+    <div class="header-item">|</div>
+    <div class="header-item">
+      <a href="resources.html">Resources</a>
+    </div>
+`;
