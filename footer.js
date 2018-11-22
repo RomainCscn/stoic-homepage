10432e8d768eb71fd778254064e99b9e0f8b86b1
901786 Cascino Romain <romain.cascino@gmail.com>
2018-11-22 13:30:00 +0700

    Improve style, add footer and new pages

---
+++ footer.js
@@ -0,0 +1,10 @@
+const footerContent = `Made by Romain Cascino &#8211; 2018
+  <span>
+    <a href="index.html">Home</a>
+  </span>  
+  <span>
+    <a href="about.html">About</a>
+  </span>
+  <span>
+    <a href="resources.html">Resources</a>
+  </span>`;
