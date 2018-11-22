aace7ab87af67b169cde2650cfd07e61dcd4bf22
901786 Cascino Romain <romain.cascino@gmail.com>
2018-11-22 23:20:24 +0700

    Add active link on nav

--- nav.js
+++ nav.js
@@ -4,14 +4,14 @@ const navContent = `
     </div>
     <div class="nav-item">|</div>
     <div class="nav-item">
-      <a href="about.html">About</a>
+      <a class="about" href="about.html">About</a>
     </div>
     <div class="nav-item">|</div>
     <div class="nav-item">
-    <a href="resources.html">Resources</a>
+    <a class="resources" href="resources.html">Resources</a>
     </div>
     <div class="nav-item">|</div>
     <div class="nav-item">
-      <a href="customize.html">Customize</a>
+      <a class="customize" href="customize.html">Customize</a>
     </div>
 `;
