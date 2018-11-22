7c6b0e15e632a6bf57271d72e06a515df26fd7eb
901786 Cascino Romain <romain.cascino@gmail.com>
2018-11-22 23:10:32 +0700

    Refactor header to nav, add meta description

---
+++ nav.js
@@ -0,0 +1,17 @@
+const navContent = `
+    <div class="nav-item">
+      <a href="index.html">Home</a>
+    </div>
+    <div class="nav-item">|</div>
+    <div class="nav-item">
+      <a href="about.html">About</a>
+    </div>
+    <div class="nav-item">|</div>
+    <div class="nav-item">
+    <a href="resources.html">Resources</a>
+    </div>
+    <div class="nav-item">|</div>
+    <div class="nav-item">
+      <a href="customize.html">Customize</a>
+    </div>
+`;
