2047c9934fafb26afefd767b2640dd5c62a4b1b9
901786 Cascino Romain <romain.cascino@gmail.com>
2018-11-22 14:38:33 +0700

    Improve style

--- footer.js
+++ footer.js
@@ -1,10 +1,14 @@
-const footerContent = `Made by Romain Cascino &#8211; 2018
-  <span>
-    <a href="index.html">Home</a>
-  </span>  
-  <span>
-    <a href="about.html">About</a>
-  </span>
-  <span>
-    <a href="resources.html">Resources</a>
-  </span>`;
+const footerContent = `
+    <div class="footer-item">
+      <a href="index.html">Home</a>
+    </div>  
+    <div class="footer-item">
+      <a href="about.html">About</a>
+    </div>
+    <div class="footer-item">
+      <a href="resources.html">Resources</a>
+    </div>
+    <div class="footer-item">
+      &#8211; <em>Romain Cascino</em> &#8211; 2018
+    </div>
+`;
